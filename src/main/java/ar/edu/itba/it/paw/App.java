d7c765ce5bad8b1949205e9bdbbbdd8b1f03eea8
Cristian Pereyra <criis.pereyra@gmail.com>
2012-03-29 22:49:35 -0300

    pom base creado

---
+++ src/main/java/ar/edu/itba/it/paw/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.it.paw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
