6a7babc1607ddec4b8042103aeb67c8802e686df
Nloreti <@nloreti@gmail.com>
2012-04-01 19:44:34 -0300

    Se duplico un metodo, fixed

--- src/main/java/ar/edu/itba/it/paw/model/services/ServiceProvider.java
+++ src/main/java/ar/edu/itba/it/paw/model/services/ServiceProvider.java
@@ -10,14 +10,6 @@ public class ServiceProvider {
 	private ServiceProvider() {
 	}
 
-	public static PropertyService getPropertyService() {
-		if (propertyService == null) {
-			propertyService = new PropertyService(DaoProvider.getPropertyDao());
-		}
-
-		return propertyService;
-	}
-
 	public static UserService getUserService() {
 		if (userService == null) {
 			userService = new UserService(DaoProvider.getUserDao());
