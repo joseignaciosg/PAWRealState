672417db9e2ba960746e4a6e124a9a7eb329968c
José Galindo <joseignaciosg9@gmail.com>
2012-04-10 18:46:40 -0300

    optinal request 2 improved

--- src/main/webapp/assets/js/application.js
+++ src/main/webapp/assets/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
 	$('input, textarea').placeholder();
+	$('.tabs').button();
+	$('.tabs').click(function(e) {
+		e.preventDefault();
+	});
+	$("#remember_session").click(function() {
+		$("#remember").val("session");
+	});
+	$("#remember_name").click(function() {
+		$("#remember").val("name");
+	});
 });
+
