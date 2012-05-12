78312cf01b77771fab25f091fd1d05ece7ad487a
Cristian Pereyra <criis.pereyra@gmail.com>
2012-05-12 13:23:18 -0300

    index on development: 39cc8d2 Merge branch 'issue/38' into development

---
+++ src/test/java/ar/edu/itba/it/paw/repositories/HibernatePropertyRepositoryTest.java
@@ -0,0 +1,33 @@
+package ar.edu.itba.it.paw.repositories;
+
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ar.edu.itba.it.paw.BaseTest;
+import ar.edu.itba.it.paw.domain.entities.Property;
+import ar.edu.itba.it.paw.domain.entities.User;
+import ar.edu.itba.it.paw.domain.repositories.impl.HibernatePropertyRepository;
+import ar.edu.itba.it.paw.domain.repositories.impl.HibernateUserRepository;
+
+public class HibernatePropertyRepositoryTest extends BaseTest {
+	@Autowired
+	HibernateUserRepository userRepository;
+
+	@Autowired
+	HibernatePropertyRepository propertyRepository;
+
+	@Autowired
+	SessionFactory factory;
+
+	@Test
+	public void searchTest() {
+		final User u = new User("name", "username", "bla", "bla", "bla", "bla");
+		this.userRepository.save(u);
+
+		final Property property = new Property(Property.Type.APARTMENT,
+				Property.Operation.RENT, "Flores", "Pedernera 35", 1233, 1, 23,
+				23, 12, null, null, "", u);
+
+	}
+}
