84f9edcb487f9e6e876eb620f2cfebd840f2bf35
José Galindo <joseignaciosg9@gmail.com>
2012-05-23 17:00:00 -0300

    i

--- src/main/java/ar/edu/itba/it/paw/domain/entities/realStateView.java
+++ src/main/java/ar/edu/itba/it/paw/domain/entities/realStateView.java
@@ -1,13 +1,13 @@
 package ar.edu.itba.it.paw.domain.entities;
 
-public class RealStateView {
+public class realStateView {
 
 	private Integer photoID;
 	private String agencyName;
 	private Integer id;
 	private Integer propCount;
 
-	public RealStateView(final Integer photoID, final String agencyName,
+	public realStateView(final Integer photoID, final String agencyName,
 			final Integer id, final long propCount) {
 		this.photoID = photoID;
 		this.agencyName = agencyName;
